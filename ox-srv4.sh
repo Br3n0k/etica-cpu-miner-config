0552a74113203fa293bc025cad037604051b7d03
Br3n0k <br3n0k@gmail.com>
2025-08-27 20:41:29 -0300

    test directly

---
+++ ox-srv4.sh
@@ -0,0 +1,13 @@
+cd /root
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz
+
+tar -xzvf xmrig-6.24.0-linux-static-x64.tar.gz
+
+rm xmrig-6.24.0-linux-static-x64.tar.gz
+
+mv xmrig-6.24.0 xmrig
+
+cd xmrig
+
+nohup ./xmrig -a rx/0 -o eticapool.com:9999 -u 0x6e9c6f5e65b0df232f0c9986ada8101686019024.Ox-srv4 -p x -k &
